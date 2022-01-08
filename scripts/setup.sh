57eb3bc6b1e8071c2d878476980560fb6ef89534
kimsooyoung <tge1375@naver.com>
2022-01-08 21:38:47 +0900

    Fix : cloning repo location

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,9 @@ echo "Installing Neuron Startup Menu..."
 
 if [ ! -d $HOME/ros_menu ]; then
     if [ -z $MENU_VERSION ]; then
-        git clone https://github.com/Adlink-ROS/ros_menu.git $HOME/ros_menu
+        git clone https://github.com/kimsooyoung/ros_menu.git $HOME/ros_menu
     else
-        git clone https://github.com/Adlink-ROS/ros_menu.git -b $MENU_VERSION $HOME/ros_menu
+        git clone https://github.com/kimsooyoung/ros_menu.git -b $MENU_VERSION $HOME/ros_menu
     fi
     cd $HOME/ros_menu
     ./install.sh $MENU_CONFIG
