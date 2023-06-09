f680a17853e2a20dbf84cc2efe5fa0ba4851d04b
kimsooyoung <tge1375@naver.com>
2023-06-09 19:55:17 +0900

    renewal for ubuntu 22.04

--- install.sh
+++ install.sh
@@ -2,46 +2,72 @@
 
 # Platform detection
 shell=`echo $SHELL | awk -F '/' '{print $NF}'`
-if [[ $(grep 20.04 /etc/issue) ]]; then
+ubuntu_version=`lsb_release -r | cut -f2`
+if [[ $ubuntu_version == 18.04 ]]; then
+    ros1_distro="melodic"
+    ros2_distro="dashing"
+    config_file=./yaml/ros_menu_18.04.yaml
+elif [[ $ubuntu_version == 20.04 ]]; then
     ros1_distro="noetic"
     ros2_distro="foxy"
     config_file=./yaml/ros_menu_20.04.yaml
+elif [[ $ubuntu_version == 22.04 ]]; then
+    # no ros1 distro anymore
+    ros1_distro="none"
+    ros2_distro="humble"
+    config_file=./yaml/ros_menu_22.04.yaml
 else
-    ros1_distro="melodic"
-    ros2_distro="eloquent"
-    config_file=./yaml/ros_menu_18.04_OpenSG.yaml
+    echo "Sorry, we don't support Ubuntu $ubuntu_version."
+    exit
 fi
 
-# ROS environment installation
-echo -n "Do you want to install ROS automatically? (y/N): "
-read ros_install
-if [ "$ros_install" '==' "y" ] || [ "$ros_install" '==' "Y" ];
+if [ "$USE_CONTAINER" '==' "True" ] || [ "$USE_CONTAINER" '==' "TRUE" ]
 then
-    echo -n "Do you want to install ROS Desktop? (y/N): "
-    read ros_desktop_install
-    if [ "$ros_desktop_install" '==' "y" ] || [ "$ros_desktop_install" '==' "Y" ];
-    then
-        # Install ROS 1
-        ./scripts/install_${ros1_distro}.sh
-
-        # Install ROS 2
-        ./scripts/install_${ros2_distro}.sh
-    else
-        # Install ROS 1
-        ./scripts/install_${ros1_distro}_base.sh
-
-        # Install ROS 2
-        ./scripts/install_${ros2_distro}_base.sh
-    fi
-
-    # Install ROS dependencies and related packages
-    ./scripts/install_dep.sh
-
+    echo "We won't install ROS / ROS 2 in your host since you use container instead."
+    echo "The container will be installed in the first time you run it."
 else
-    echo "Skip installing ROS"
+    # ROS environment installation
+    echo -n "Do you want to install ROS automatically? (y/N): "
+    read ros_install
+    if [ "$ros_install" '==' "y" ] || [ "$ros_install" '==' "Y" ];
+    then
+        echo -n "Do you want to install ROS Desktop? (y/N): "
+        read ros_desktop_install
+        if [ "$ros_desktop_install" '==' "y" ] || [ "$ros_desktop_install" '==' "Y" ];
+        then
+            # Install ROS 1
+            ./scripts/install_${ros1_distro}.sh
+
+            # Install ROS 2
+            ./scripts/install_${ros2_distro}.sh
+        else
+            # Install ROS 1
+            ./scripts/install_${ros1_distro}_base.sh
+
+            # Install ROS 2
+            ./scripts/install_${ros2_distro}_base.sh
+        fi
+
+        # Install ROS dependencies and related packages
+        ./scripts/install_dep.sh
+
+    else
+        echo "Skip installing ROS"
+    fi
 fi
 
 # Install ROS menu and config file
+if [ -f ~/ros_menu/config.yaml ]; then
+    echo  "ROS Startup Menu was already installed!"
+    echo -n "Do you want to reinstall? (Your setting in the file config.yaml would be over written.) (y/N): "
+    read over_write
+else
+    over_write="y"
+fi
+if [ ! "$over_write" '==' "y" ] && [ ! "$over_write" '==' "Y" ]; then
+    echo "Skip installing ROS Startup Menu!"
+    exit
+fi
 rm -f ~/.ros_menu
 ln -s `pwd` ~/.ros_menu
 if [[ -n $1 ]]; then
@@ -76,13 +102,14 @@ alias rosmelo="source /opt/ros/melodic/setup.bash"
 alias roseloq='source /opt/ros/eloquent/setup.bash && source ./install/setup.bash && export PYTHONPATH=/opt/ros/eloquent/lib/python3.6/site-packages'
 alias rosfoxy='source /opt/ros/foxy/setup.bash && source ./install/setup.bash && source ./install/local_setup.bash'
 alias rosdinstall='rosdep install -y -r -q --from-paths src --ignore-src --rosdistro'
-# Neuron Startup Menu #
+
+# ROS Startup Menu #
 ros_bashrc_path=~/.ros_menu/ros_bashrc
 if [ -f \$ros_bashrc_path ]; then
     source \$ros_bashrc_path
 fi
-# End of Neuron Startup Menu #
+# End of ROS Startup Menu #
 EOF
 fi
 
-echo "Neuron Startup Menu installed successfully"
+echo "ROS Startup Menu installed successfully"
--- ros_menu.py
+++ ros_menu.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 # Common variables
-host_sourcefilename = "/tmp/host_sourcefile.txt"
-container_sourcefilename = "/tmp/container_sourcefile_%s.txt"
+host_sourcefilename = '/tmp/host_sourcefile.txt'
+container_sourcefilename = '/tmp/container_sourcefile_%s.txt'
 
 # Read YAML file.
 f = open('%s' % sys.argv[1], 'r')
@@ -14,12 +14,12 @@ else:
     source_file = yaml.load(f)
 f.close()
 
-if (source_file['Config']['menu_enable'] != True):
+if source_file["Config"]["menu_enable"] is not True:
     sys.exit(0)
 
 # Generate Menu
 keys = list(source_file['Menu'])
-print('********* RoadBalance Startup Menu for ROS ***********')
+print('********** RoadBalance Startup Menu for ROS **********')
 print('* Usage: Please select the following options to load *')
 print('*        ROS environment automatically.              *')
 print('******************************************************')
@@ -27,8 +27,8 @@ print('0) Do nothing')
 choose_dict = {}
 for key in keys:
     if str(source_file['Menu'][key]['option_num']) in list(choose_dict):
-        raise SyntaxError('Some option numbers(option_num) in the YAML file are duplicated.')
-        sys.exit(0)
+        raise SyntaxError(
+            'Some option numbers(option_num) in the YAML file are duplicated.')
     print('%s) %s ' % (source_file['Menu'][key]['option_num'], key))
     choose_dict['%s' % source_file['Menu'][key]['option_num']] = key
 print('h) Help')
@@ -60,7 +60,7 @@ choose = choose_dict[ros_option]
 
 def read_cmds():
     ret_cmds = ""
-    if (source_file['Menu'][choose]['cmds'] is not None):
+    if source_file['Menu'][choose]['cmds'] is not None:
         for cmds in source_file['Menu'][choose]['cmds']:
             ret_cmds += cmds + '\n'
     return ret_cmds
@@ -68,14 +68,15 @@ def read_cmds():
 
 def source_ros1():
     current_ip = os.popen("hostname -I | awk '{print $1}'").read().split('\n')[0]
-    if (len(current_ip) == 0):
+    if len(current_ip) == 0:
         current_ip = '127.0.0.1'
     ros_master_uri = source_file['Menu'][choose]['master_ip']
-    if (ros_master_uri is None):
+    if ros_master_uri is None:
         ros_master_uri = current_ip
     source_ros = 'source %s/setup.$shell' % source_file['Menu'][choose]['ros1_path']
     export_ip = 'export ROS_IP=%s' % current_ip
-    export_ros_master_uri = 'export ROS_MASTER_URI=http://%s:11311' % ros_master_uri.rstrip("\n")
+    export_ros_master_uri = 'export ROS_MASTER_URI=http://%s:11311' \
+                                                     % ros_master_uri.rstrip("\n")
     print('* ROS_IP=%s' % current_ip.rstrip('\n'))
     print('* ROS_MASTER_URI %s' % ros_master_uri.rstrip('\n'))
     print('------------------------------------------------------')
@@ -84,10 +85,12 @@ def source_ros1():
 
 def source_ros2():
     ros_domain_id = source_file['Menu'][choose]['domain_id']
-    if (ros_domain_id is None):
+    if ros_domain_id is None:
         ros_domain_id = int(source_file['Config']['default_ros_domain_id'])
-    source_ros = 'source %s/local_setup.$shell' % source_file['Menu'][choose]['ros2_path']
-    source_colcon = 'source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.$shell'
+    source_ros = 'source %s/local_setup.$shell' \
+                                    % source_file['Menu'][choose]['ros2_path']
+    source_colcon = \
+            'source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.$shell'
     export_domain_id = 'export ROS_DOMAIN_ID=%d' % ros_domain_id
     print('* ROS_DOMAIN_ID = %d' % ros_domain_id)
     print('------------------------------------------------------')
@@ -95,17 +98,20 @@ def source_ros2():
 
 
 def check_bridge():
-    ret_string  = "if [ ! -d %s/lib/ros1_bridge ]; then\n" % source_file['Menu'][choose]['ros2_path']
+    ret_string  = "if [ ! -d %s/lib/ros1_bridge ]; then\n" \
+                                     % source_file['Menu'][choose]['ros2_path']
     ret_string += "  echo 'You need to install ros1_bridge first.'\n"
-    ret_string += "  echo 'Installation command: sudo apt install ros-%s-ros1-bridge'\n" % source_file['Menu'][choose]['ros2_version_name']
-    ret_string += "  exit 1\n"
+    ret_string += "  echo 'Installation command:" + \
+                  " sudo apt install ros-%s-ros1-bridge'\n" \
+                     % source_file['Menu'][choose]['ros2_version_name']
     ret_string += "fi\n"
     return ret_string
 
 
 def create_ros_sourcefile(filename):
     ros_source_file = open(filename, 'w')
-    ros_source_file.write("shell=`echo $SHELL | awk -F '/' '{print $NF}' |  sed 's/\/bin\///g' `\n")
+    ros_source_file.write(
+        "shell=`echo $SHELL | awk -F '/' '{print $NF}' |  sed 's/\/bin\///g' `\n")
     ros_source_file.write("PS1=\"(%s) $PS1\"\n" % choose)
     if (source_file['Menu'][choose]['ROS_version'] == 1):
         ros_source_file.write(source_ros1()+read_cmds())
@@ -116,12 +122,13 @@ def create_ros_sourcefile(filename):
     ros_source_file.close()
 
 
-if ( 'container' in source_file['Menu'][choose] ):
+if 'container' in source_file['Menu'][choose]:
     container_sourcefilename = container_sourcefilename % ros_option
     create_ros_sourcefile(container_sourcefilename)
     with open(host_sourcefilename, "w") as f:
         # Use $ROS_OPTION to select the environment in docker container
         # Use $CONTAINER to select which container to use
-        f.write("ROS_OPTION=%s CONTAINER=%s ~/ros_menu/scripts/docker_run.sh" % (ros_option, source_file['Menu'][choose]['container']))
+        f.write('ROS_OPTION=%s CONTAINER=%s ~/ros_menu/scripts/docker_run.sh'
+                    % (ros_option, source_file['Menu'][choose]['container']))
 else:
     create_ros_sourcefile(host_sourcefilename)
